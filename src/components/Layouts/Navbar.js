c0112c88c483980f0780a9d961b5b76403333c7b
Michael Antoni <michaelantoni.cs@gmail.com>
2020-05-17 13:28:43 +0800

    remove navbar animation

--- src/components/Layouts/Navbar.js
+++ src/components/Layouts/Navbar.js
@@ -4,7 +4,7 @@ function Navbar() {
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
             <div className="container">
-                <a className="navbar-brand wow fadeInLeft" href="/"><i className="fa fa-globe-africa"></i> Weather App</a>
+                <a className="navbar-brand" href="/"><i className="fa fa-globe-africa"></i> Weather App</a>
                 <button className="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
