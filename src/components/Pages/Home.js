f2b5c396eb68ec2bc6c653d2e56ca18069550e8a
Michael Antoni <michaelantoni.cs@gmail.com>
2020-05-19 23:14:52 +0800

    update logo image from photos.google

--- src/components/Pages/Home.js
+++ src/components/Pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
         <Fragment>
             <div class="container text-center mb-5">
                 <div>
-                    <img src="assets/img/weather.gif" className="img-fluid" alt="img" width="300px"/>
+                    <img src="https://lh3.googleusercontent.com/ogD8EJ413GofX_85vnzkK0CpnGmi3tVWcd_Dh4jGwCmHKEpVuFgrMD72lHhgkLC9dJ-GQQ1mVdvflspWr8-P69qHNLXkhafaq8MV9-T6Getv2Y6qHN5IvQqDEMR3AJ_fm_12sytb=w2400" className="img-fluid" alt="img" width="300px"/>
                 </div>
                 <p class="lead py-3 text-white"><TypeIt options={{ loop: true, pause: 300, speed: 50, strings: [ "Search weather forecast around the world"]}}></TypeIt></p>
                 <a href="/search" className="btn btn-dark m-1 wow fadeInLeft">search now</a> &nbsp;
