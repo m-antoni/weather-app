68bd816b147212b2db599d4fa78fa1c273a81f68
Michael Antoni <michaelantoni.tech@gmail.com>
2025-12-22 07:20:18 +0800

    Update contact email address in Contact.js

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -8,7 +8,7 @@ function Contact() {
                 
                 <p>Thank you for using the app hope you enjoy it. I appreciate any comments.</p>
     
-                <div className="mt-3"> <i className="fa fa-envelope"></i>&nbsp;<a href="mailto:codehive2018@gmail.com" className="text-warning"> michaelantoni.cs@gmail.com</a> </div>
+                <div className="mt-3"> <i className="fa fa-envelope"></i>&nbsp;<a href="mailto:michaelantoni.tech@gmail.com" className="text-warning"> michaelantoni.tech@gmail.com</a> </div>
                 <div> <i className="fab fa-github"></i>&nbsp;<a href="https://github.com/m-antoni" className="text-warning" target="_blank"> github.com/m-antoni</a> </div>
                 <div>Created by:&nbsp; Michael Antoni </div>
                 <div>App Version: 1.0</div>
